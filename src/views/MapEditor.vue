17f1ee4a91d5bfb66e9a6dc063742ad54e0e9a79
DanielCheang007 <93252061+DanielCheang007@users.noreply.github.com>
2025-04-15 08:32:38 +0800

    setup map editor

---
+++ src/views/MapEditor.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from "vue";
+
+const currentLevel = ref(1);
+</script>
+<template>
+  <div class="map-editor">
+    <header>
+      <h2>
+        Map Editor - Level
+        {{ currentLevel }}
+      </h2>
+
+      <div class="level-selector">
+        <button
+          v-for="i in 3"
+          :key="i"
+          :class="{ active: currentLevel === i }"
+          @click="currentLevel = i"
+        >
+          Level {{ i }}
+        </button>
+      </div>
+    </header>
+    <main></main>
+    <footer>
+      <button>Play Demo</button>
+    </footer>
+  </div>
+</template>
